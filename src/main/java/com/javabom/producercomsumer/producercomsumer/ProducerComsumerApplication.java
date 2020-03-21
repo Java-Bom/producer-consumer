390606414e0080ea4e0fb6e154ad52f655ba3757
최유성 <chldbtjd7530@naver.com>
2020-03-21 16:04:17 +0900

    이벤트 시작

---
+++ src/main/java/com/javabom/producercomsumer/producercomsumer/ProducerComsumerApplication.java
@@ -0,0 +1,13 @@
+package com.javabom.producercomsumer.producercomsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerComsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerComsumerApplication.class, args);
+    }
+
+}
